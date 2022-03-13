00a5c67795cd30a33a51aeaf5a5937733f11dc53
jackyang <jackyang@universalec.com>
2022-03-13 13:40:16 +0800

    初始化UI React專案

---
+++ ui/src/App.tsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const App = () => {
+
+    const [name, setName] = useState<string>("");
+
+    return (
+        <div>
+            Hello World!!
+        </div>
+    );
+}
+
+export default App;
